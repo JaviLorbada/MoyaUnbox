993c7ec71b1a475a803c093d918269d9ad7775ec
JaviLorbada <javugi@gmail.com>
2016-08-15 16:00:23 +0200

    Basic project setup. Three frameworks, MoyaUnbox, RxMoyaUnbox and ReactiveMoyaUnbox

---
+++ Supporting Files/MoyaUnbox.h
@@ -0,0 +1,16 @@
+//
+//  MoyaUnbox.h
+//  MoyaUnbox
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MoyaUnbox.
+FOUNDATION_EXPORT double MoyaUnbox_VersionNumber;
+
+//! Project version string for MoyaUnbox.
+FOUNDATION_EXPORT const unsigned char MoyaUnbox_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MoyaUnbox/PublicHeader.h>
+
+
